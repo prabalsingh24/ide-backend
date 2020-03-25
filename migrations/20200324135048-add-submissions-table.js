cf068ee45b9b4e36ceeec61a4b5e0426a704d741
Jatin Katyal <jatin.katyal13@gmail.com>
2020-03-25 23:55:02 +0530

    create submissions table migration

---
+++ migrations/20200324135048-add-submissions-table.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('submissions', {
+      id: {
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      judge_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false
+      },
+      is_completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      is_successful: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      outputs: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('submissions')
+  }
+};
